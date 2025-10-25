f82872a605ea614855a549a85581ff6c777c0002
Heach8 <hkhasanoglu@gmail.com>
2025-10-25 22:50:11 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -70,3 +70,8 @@ function App(){
 }
 
 createRoot(document.getElementById('root')).render(<App/>)
+
+import axios from "axios";
+import { API_BASE_URL } from "./config";
+
+axios.defaults.baseURL = API_BASE_URL;
